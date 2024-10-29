fdb090d797bcc1122dcae30a5d21a28eaeea5194
Hussain Mansoori <mansoorih980@gmail.com>
2024-10-29 19:57:34 +0530

    Revert "CRUD APP"
    This reverts commit 54a0724485486fbd3d201995b8db43c05916e0a5.

--- src/main/java/com/hussain/rest/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hussain.rest.repository;
-
-import com.hussain.rest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
