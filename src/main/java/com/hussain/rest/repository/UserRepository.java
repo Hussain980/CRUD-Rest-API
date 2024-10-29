54a0724485486fbd3d201995b8db43c05916e0a5
Hussain Mansoori <mansoorih980@gmail.com>
2024-10-29 19:50:35 +0530

    CRUD APP

---
+++ src/main/java/com/hussain/rest/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hussain.rest.repository;
+
+import com.hussain.rest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
